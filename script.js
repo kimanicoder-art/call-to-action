8dfa7f67e0855a40e40c6a0a7d0a6dcae406ecc2
kimanicoder-art <kimaniits@gmail.com>
2024-09-20 19:59:00 +0300

    changed

--- script.js
+++ script.js
@@ -12,19 +12,20 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlide");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-        let button = slides[i].querySelector('.cta-button');
-        button.style.animation = 'none'; // Reset animation
-        button.offsetHeight; // Trigger reflow
-        button.style.animation = ''; // Reapply animation
     }
     slides[slideIndex-1].style.display = "block";
 }
 
-// Optional: Auto play the slideshow
+function openWindow() {
+    window.open('register.html', '_blank');
+}
+
+// Optional: Auto-play the slideshow
 setInterval(() => {
     plusSlides(1);
 }, 15000); // Change image every 15 seconds
+
